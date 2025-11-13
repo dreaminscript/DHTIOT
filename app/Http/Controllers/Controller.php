4d8a6b0d4f7aba3fdc27c89cc736ee750f999b94
hanifa <hanifafebriana326@gmail.com>
2025-11-13 10:30:45 +0700

    kedua

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,45 @@
+controller
 <?php
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Dht22;
+use Illuminate\Http\Request;
+
+class Dht22Controller extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $dht = Dht22::count();
+        if($dht == 0) {
+            Dht22::create([
+                'temperature' => 0,
+                'humidity' => 0
+            ]);
+        }
+    }
+    public function updateData($tmp, $hmd)
+    {
+     $dht = Dht22::first();
+     $dht->temperature = $tmp;
+     $dht->humidity = $hmd;
+     $dht->save();   
+
+     return response()->json(['message' => 'Data updated successfully']);
+    }
+
+public function getData()
+{
+    $dht =  Dht22::first();
+    $status = ($dht->temperature > 30) ? 'on' : 'off';
+
+    return response()->json([
+        'lamp' => $status,  
+        'temperature' => $dht->temperature,
+        'humidity' => $dht->humidity,
+    ]);
+}
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/update-data/{tmp}/{hmd}', [Dht22Controller::class, 'updateData']);
 Route::get('/get-data', [Dht22Controller::class, 'getData']);
